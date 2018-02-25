193e8efca301fc8b1540201b1b9450b9aacd3749
marina <marina.shatinskaya@gmail.com>
2018-02-25 17:01:27 +0300

    solution 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  array.sort();
+  array.sort(compareNumbers);
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+    
+  var counter =1;
+  var maxCount = (array.length ==0) ? 0 : 1;
+  for (var i=0; i<array.length-1; i++){
+    if (array[i+1]==array[i]+1){
+      //если +1, то счетчик +1
+      counter++;
+    } else if (array[i+1]==array[i]) {
+      //если равно, то пропускаем (ничего не делаем)
+    } else {
+      //в остальных случаях выполняем код ниже
+            if (maxCount<counter){
+              maxCount = counter;
+            }
+            counter = 1;
+    }
+        
+  }
+     
+  return maxCount;
 }
